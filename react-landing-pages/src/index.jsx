54fcce1724289fe7c5bcb7c957fdda19d95c804c
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-01-10 16:02:33 -0300

    styled components

--- react-landing-pages/src/index.jsx
+++ react-landing-pages/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyles } from './styles/global-styles.js';
 import Home from './templates/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root'),
 );
